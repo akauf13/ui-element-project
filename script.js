589174829d4fd823b0552f5384b01ceff4ddac3c
Adam Kaufman <adamkaufman@Adams-MacBook-Pro-3.local>
2022-09-30 12:32:12 -0400

    BrBa

--- script.js
+++ script.js
@@ -3,8 +3,24 @@ const url = "https://breakingbadapi.com/api/characters";
 const container = document.querySelector(".container");
 
 function displayUI(arr) {
+  // let newArr = arr.filter(pick => pick.name !== "Lydia Rodarte-Quayle")
+  // console.log(newArr);
+  // let newChar = arr.filter((char) => char.name === "Lydia Rodarte-Quayle");
+  // console.log(newChar[0]);
+
+  // let saulChars = arr.filter(chars => chars.char_id === 112 || chars.char_id === 113 || chars.char_id === 114 || chars.char_id === 115 || chars.char_id === 116)
+  // let saulChars = arr.filter(chars => chars.char_id >= 112)
+  // console.log(saulChars);
+  arr.splice(57);
+  console.log(arr);
+
   arr.forEach((person) => {
     let occupations = person.occupation.map((occ) => `<p>${occ}</p>`).join(" ");
+
+    if (person.name === "Lydia Rodarte-Quayle") {
+      person.img =
+        "https://hips.hearstapps.com/digitalspyuk.cdnds.net/17/03/1484912975-laurafraser.JPG?crop=0.739xw:1.00xh;0.190xw,0&resize=480:*";
+    }
 
     let htmlTemplate = `
     <div class="card">
@@ -18,11 +34,35 @@ function displayUI(arr) {
       `;
     container.insertAdjacentHTML("beforeend", htmlTemplate);
   });
+
+}
+
+let rightButton = document.querySelector(".right");
+
+rightButton.addEventListener("click", handleClick);
+
+function handleClick(e) {
+  e.preventDefault();
+  container.scrollBy({
+    left: 700,
+    behavior: "smooth",
+  });
+}
+let leftButton = document.querySelector(".left");
+
+leftButton.addEventListener("click", handleClick2);
+
+function handleClick2(e) {
+  e.preventDefault();
+  container.scrollBy({
+    left: -700,
+    behavior: "smooth",
+  });
 }
 
 async function fetchCharacters() {
   let response = await axios(url);
-  console.log(response.data);
+  // console.log(response.data);
   displayUI(response.data);
 }
 fetchCharacters();
